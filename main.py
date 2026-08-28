9fbf63c8674909ad56fc3f602b5f6b6b9730a654
Cici Xing <cici393924@gmail.com>
2026-08-27 22:06:29 -0700

    Update

---
+++ main.py
@@ -0,0 +1,6 @@
+# import libraries
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import confusion_matrix
